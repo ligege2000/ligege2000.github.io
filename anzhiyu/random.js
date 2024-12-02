1e8a3c6f11c117a2a6bec0f31b1c93fc83fa2e7a
unknown <liguangliang588@outlook.com>
2024-12-02 18:29:37 +0800

    Site updated: 2024-12-02 18:29:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/02/hello-world/"];function toRandomPost(){
+var posts=["2024/12/02/这是一篇新的博文/","2024/12/02/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
